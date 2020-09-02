15540c83ea24fad3849b00dcd4cc061ae00dcfbd
wlkmanist <t.me/wlkmanist>
2020-09-03 02:14:07 +0300

    added controls.json and script to get it
    Signed-off-by: wlkmanist <t.me/wlkmanist>

--- get_cc.sh
+++ get_cc.sh
@@ -1,4 +1,5 @@
 adb pull /sdcard/Android/data/com.grarak.kerneladiutor/controls/ ./
+adb pull /data/data/com.grarak.kerneladiutor/files/controls.json ./controls.json
 [ $? -eq 0 ]  || exit -1
-zip -D -r ./KernelAdiutorCustomControlsPack- ./controls/
+zip -D -r ./KernelAdiutorCustomControlsPack- ./controls/ ./controls.json
 
