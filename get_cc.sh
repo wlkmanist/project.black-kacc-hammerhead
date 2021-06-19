8b7353767f380aaa6b1cb2fb4f86e950efb20372
wlkmanist <t.me/wlkmanist>
2021-06-19 03:19:51 +0300

    get_cc.sh update
    Signed-off-by: wlkmanist <t.me/wlkmanist>

--- get_cc.sh
+++ get_cc.sh
@@ -1,5 +1,8 @@
-adb pull /sdcard/Android/data/com.grarak.kerneladiutor/controls/ ./
+adb root
+adb remount
+
+# adb pull /sdcard/Android/data/com.grarak.kerneladiutor/controls/ ./
 adb pull /data/data/com.grarak.kerneladiutor/files/controls.json ./controls.json
 [ $? -eq 0 ]  || exit -1
-zip -D -r ./KernelAdiutorCustomControlsPack- ./controls/ ./controls.json
+zip -D -r ./KernelAdiutorCustomControlsPack- ./push_cc.sh ./controls.json
 
